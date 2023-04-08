0abedb44eeba27697f95618f6495ffba8185c0d9
AsherFYD <AsherFYD@gmail.com>
2023-04-08 23:36:55 +0800

    fetch module

--- functions/askGPT.js
+++ functions/askGPT.js
@@ -56,6 +56,7 @@ exports.handler = async function(event, context) {
 			statusCode: 200,
 			body: JSON.stringify({response: reply})
 		}
+
 	} catch (err){
 		return { statusCode: 422, body: err.stack}
 	}
