17de4c222ca5fb8663914909c7fcfa203d7ad23f
AsherFYD <AsherFYD@gmail.com>
2023-04-09 19:30:41 +0800

    remove useHistory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Header from './Header';
 import Home from './Home';
 import Resources from './Resources';
-import { Route, Routes, useHistory } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   // load messages from localstorage if there are messages stored, else initialise as empty array
