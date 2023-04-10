68f44bd369c8712f8ae2e057a05bb2ffab3715d5
AsherFYD <AsherFYD@gmail.com>
2023-04-10 23:55:19 +0800

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import Home from './Home';
 import Resources from './Resources';
@@ -6,25 +6,37 @@ import { Route, Routes } from 'react-router-dom'
 
 function App() {
   // to get the height of the screen, so that height:100vh will not be used
-  const documentHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+  // const documentHeight = () => {
+  //   const doc = document.documentElement
+  //   doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+  // }
+  // window.addEventListener("resize", documentHeight)
+  // documentHeight() // call when app is first loaded
+
+  // custom hook
+  const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+            document.documentElement.style.setProperty('--doc-height', `${window.innerHeight}px`)
+        }
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []) // no dependencies
+
+    return windowSize;
   }
-  window.addEventListener("resize", documentHeight)
-  documentHeight() // call when app is first loaded
-
-  // whenever resize event happens
-  // useEffect(() => {
-  //   const documentHeight = () => {
-  //     const doc = document.documentElement
-  //     doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
-  //   }
-
-  //   window.addEventListener("resize", documentHeight)
-  //   return () => {
-  //     window.removeEventListener("resize", documentHeight)
-  //   }
-  // }, [])
+  const { height } = useWindowSize();
 
   // load messages from localstorage if there are messages stored, else initialise as empty array
   const [messages, setMessages] = useState(
