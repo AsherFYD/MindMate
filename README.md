ad4011826ff93e03ffe86ccafcf72e7264bdabb5
AsherFYD <AsherFYD@gmail.com>
2023-04-10 23:13:43 +0800

    responsive design

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-AI therapy chatbot using gpt-3.5-turbo model
+AI therapy chatbot integrated with gpt-3.5-turbo model
+Try at: https://mindmate-chat.netlify.app/
--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import Resources from './Resources';
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
+  // const windowWidth = useRef(window.innerWidth);
+  // const windowHeight = useRef(window.innerHeight);
+  // console.log('width: ', windowWidth.current);
+  // console.log('height: ', windowHeight.current);
+
+  const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+  }
+  window.addEventListener("resize", documentHeight)
+  documentHeight()
+
   // load messages from localstorage if there are messages stored, else initialise as empty array
   const [messages, setMessages] = useState(
                                     localStorage.getItem("messages") 
